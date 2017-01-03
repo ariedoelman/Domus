043c838a6cd5095685ad01da8a7c3a44670293ad
ariedoelman <arie.doelman@gmail.com>
2017-01-03 13:01:45 +0100

    Initial commit

---
+++ Sources/App/main.swift
@@ -0,0 +1,11 @@
+import Vapor
+
+let drop = Droplet()
+
+drop.get { req in
+    return try drop.view.make("welcome", [
+    	"message": drop.localization[req.lang, "welcome", "title"]
+    ])
+}
+
+drop.run()
