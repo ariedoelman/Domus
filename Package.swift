2e45cb5db29c567f366e4e3fd8009697cff6c2b8
ariedoelman <arie.doelman@gmail.com>
2017-01-05 21:24:22 +0100

    Added GrovePiIO

--- Package.swift
+++ Package.swift
@@ -3,7 +3,8 @@ import PackageDescription
 let package = Package(
     name: "Domus",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3)
+      .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3),
+      .Package(url: "https://github.com/ariedoelman/GrovePiIO.git", majorVersion: 1)
     ],
     exclude: [
         "Config",
